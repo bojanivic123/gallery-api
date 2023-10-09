e97ca85c0c10eb16b324d2e3205d10c184c5947f
bojanivic123 <bojan.ivic111@gmail.com>
2023-10-09 20:40:34 +0200

    as

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -27,7 +27,7 @@ class RegisterRequest extends FormRequest
             "first_name" => "required|string|max:255",
             "last_name" => "required|string|max:255",
             "email" => "required|email|max:255|unique:users,email",
-            "password" => "required|string|min:8|regex:/^(?=.*[0-9])/|confirmed"
+            "password" => "required|string|min:8|max:255|regex:/^(?=.*[0-9])/|confirmed"
         ]; 
     }
 }
--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Gallery extends Model
 {
     use HasFactory;
-    // protected $fillable = ["name", "description", "urls", "user_id"];
+    protected $fillable = ["name", "description", "urls", "user_id"]; 
 
     public function user() {
         return $this->belongsTo(User::class);
