a3d05c08bed6e791664f760d8b457a1d090dbeef
bojanivic123 <bojan.ivic111@gmail.com>
2023-10-09 16:50:10 +0200

    mc

--- app/Http/Requests/UpdateCommentRequest.php
+++ app/Http/Requests/UpdateCommentRequest.php
@@ -13,7 +13,7 @@ class UpdateCommentRequest extends FormRequest
      */
     public function authorize()
     {
-        return true; 
+        return false; 
     }
 
     /**
@@ -24,7 +24,7 @@ class UpdateCommentRequest extends FormRequest
     public function rules()
     {
         return [
-            "description" => "max:1000|string"
+            // 
         ];
     }
 }
